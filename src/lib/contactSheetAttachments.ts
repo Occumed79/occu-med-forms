2d6144720d11b06251b56d82a0d30e96fc6a66ef
Occumed79 <Alex.Ayvazian@logan.edu>
2026-07-02 01:25:59 -0700

    Use full Occu-Med contact attachment fields

--- src/lib/contactSheetAttachments.ts
+++ src/lib/contactSheetAttachments.ts
@@ -1,16 +1,28 @@
 export type AttachmentPage = { title: string; fields: Array<{ label: string; value: string }> };
 
 export const occuMedContactSheetAttachment = (): AttachmentPage => ({
-  title: "Occu-Med Contact Sheet",
+  title: "Occu-Med Contact Information",
   fields: [
-    { label: "Organization", value: "Occu-Med" },
-    { label: "Primary Contact Name", value: "" },
-    { label: "Title", value: "" },
-    { label: "Email", value: "" },
-    { label: "Phone", value: "" },
-    { label: "Fax", value: "" },
-    { label: "Address", value: "" },
-    { label: "Billing Email", value: "" },
+    { label: "Company Name", value: "Occu-Med, LTD" },
+    { label: "Address", value: "2121 W Bullard Ave" },
+    { label: "City, State Zip", value: "Fresno, CA 93711" },
+    { label: "Country", value: "United States of America" },
+    { label: "Telephone", value: "(559) 435-2800" },
+    { label: "Fax", value: "(800) 262-2863" },
+    { label: "Monday", value: "7:30am to 4:30pm PST" },
+    { label: "Tuesday", value: "7:30am to 4:30pm PST" },
+    { label: "Wednesday", value: "7:30am to 4:30pm PST" },
+    { label: "Thursday", value: "7:30am to 4:30pm PST" },
+    { label: "Friday", value: "7:30am to 4:30pm PST" },
+    { label: "Saturday", value: "CLOSED" },
+    { label: "Sunday", value: "CLOSED" },
+    { label: "Network Management - Name | Title | Telephone | Email", value: "Matt Caskey | Director | x104 | mcaskey@occu-med.com" },
+    { label: "Provider Relations - Name | Title | Telephone | Email", value: "Liz Zecchini | Manager | x153 | elizabeth.zecchini@occu-med.com" },
+    { label: "EXAMQA - Name | Title | Telephone | Email", value: "Dana Tamayo | Director | x159 | dtamayo@occu-med.com" },
+    { label: "Communications - Name | Title | Telephone | Email", value: "x172 | communicationsmanager@occu-med.com" },
+    { label: "Scheduling - Name | Title | Telephone | Email", value: "Liz Mathies | Director | x151 | elizabeth.mathies@occu-med.com" },
+    { label: "Operations - Name | Title | Telephone | Email", value: "Chase Coyle | Director | x102 | ccoyle@occu-med.com" },
+    { label: "Finance - Name | Title | Telephone | Email", value: "Alyson Tillery | Director | x116 | atillery@occu-med.com" },
   ],
 });
 
